d89f6cc68e59b0e80c432580fc20c6e7af5dacda
lknkimngan <lekimngan22102002@gmail.com>
2024-03-23 01:36:31 +0700

    đổi mk thành công sẽ quay về trang login, thiết kế giao diện của trang info profile và trang edit info

--- app/src/main/java/com/example/android_social_media/InfoProfile.java
+++ app/src/main/java/com/example/android_social_media/InfoProfile.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ForgetPassword1 extends AppCompatActivity {
+public class InfoProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forget_password1);
+        setContentView(R.layout.activity_info_profile);
     }
 }
