6c2b68ce6f482b676f9ed3aa06c9e808086d63ef
vothimy2002 <vothimy2002vx@gmail.com>
2024-04-10 21:54:34 +0700

    chuyển story_post thành fragment

---
+++ app/src/main/java/com/example/android_social_media/fragment/story_postFragment.java
@@ -0,0 +1,36 @@
+package com.example.android_social_media.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android_social_media.R;
+
+
+public class story_postFragment extends Fragment {
+
+
+
+    public story_postFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      View view=inflater.inflate(R.layout.fragment_story_post,container,false);
+
+      return view;
+
+    }
+}
